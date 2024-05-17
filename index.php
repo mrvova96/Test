b530db36f36742945ebaad32629dfcfa1f685ada
Vladimir Platonov <platonow.vova2010@yandex.ru>
2024-05-17 07:37:11 +0300

    Test commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<div class="cont_offer">
+    <div class="side offer">
+        <div class="text-offer1">Забота о вашем питомце</div>
+        <div class="text-offer2">Любимцы заслуживают только лучшего!</div>
+        <button class="btn-offer" id="first-button">Записаться</button>
+    </div>
+</div>
+
+</body>
+</html>
